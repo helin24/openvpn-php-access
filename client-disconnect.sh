c57ba903f0ad938b95a796dda3bd68b6595c5731
Scott Crooks <scott.crooks@gmail.com>
2016-05-02 10:20:12 -0700

    BASH scripts called from OpenVPN process

---
+++ client-disconnect.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#======================================================================================================================
+# vim: softtabstop=4 shiftwidth=4 expandtab fenc=utf-8 spell spelllang=en cc=120
+#======================================================================================================================
+#
+#          FILE: client-connect.sh
+#
+#   DESCRIPTION: Shell executable used during client connect; parameters passed to openvpn.php
+#
+#          BUGS: https://github.com/helin24/openvpn-php-access/issues
+#
+#       LICENSE: GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+#  ORGANIZATION: Helin Shiah
+#       CREATED: Mon May 2 09:38:36 PDT 2016
+#======================================================================================================================
+
+## Parameters
+
+USERTEMPFILE=$1
+PHPCLI=$(which php)
+SUDO=$(which sudo)
+
+## Sanity check
+
+if [[ -z "${PHPCLI}" || "${PHPCLI}" = "" ]]; then echo "ERROR: PHP NOT INSTALLED" && exit 1 ; fi
+if [[ -z "${SUDO}" || "${SUDO}" = ""  ]]; then echo "ERROR: SUDO NOT FOUND" && exit 1 ; fi
+
+## Black magic
+
+${SUDO} --preserve-env ${PHP} $(pwd)/openvpn.php ${USERTEMPFILE} disconnect ${username}
